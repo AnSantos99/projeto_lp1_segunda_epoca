90acad4ea6187b3db16bdc2b894e03cc0fde02c3
nanilev <dianuski99@gmail.com>
2019-06-30 12:46:16 +0100

    Add project files.

---
+++ lp1_projetoFinal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lp1_projetoFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
