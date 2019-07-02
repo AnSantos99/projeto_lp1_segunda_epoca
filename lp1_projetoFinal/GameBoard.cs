f8d90ab3e3bf660dabb28c2eacf85fce3016fef1
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-07-02 11:22:32 +0100

    small changes

--- lp1_projetoFinal/GameBoard.cs
+++ lp1_projetoFinal/GameBoard.cs
@@ -7,12 +7,8 @@ namespace lp1_projetoFinal
     class GameBoard
     {
 
-<<<<<<< HEAD
-        static int[] gameBoard;
-=======
        // public  int ColSize { get; set; }
         //public  int RowSize { get; set; }
->>>>>>> upstream/master
 
         static int[,] gameBoard;
 
@@ -26,15 +22,6 @@ namespace lp1_projetoFinal
             int RowSize = 9;
 
             // Declare Variables BOARD
-<<<<<<< HEAD
-                        
-            //const string MAPXCOORD = ("      1     2    3    4    5  ");
-            const string TOPLINE = "~~~~~ ~~~~~ ~~~~~ ~~~~~ ~~~~~ ~~~~~ ~~~~~";
-            const string SIDELINE = "    |    |    |    |    |    |";
-            string[] yCoordLetters = { "A", "B", "C", "D", "E" };
-
-            //Console.WriteLine($"{MAPXCOORD}");
-=======
             gameBoard = new int[ColSize, RowSize];
 
             // print the top row
@@ -42,7 +29,6 @@ namespace lp1_projetoFinal
             for (uint i = 0; i < RowSize; i++) Console.Write($"_[{i}]_|");
 
             Console.WriteLine();
->>>>>>> upstream/master
 
             // begin cycle to set all the rows right
             for (uint y = 0; y < ColSize; y++)
