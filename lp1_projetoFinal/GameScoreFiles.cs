99a2ed90a716a143e02960448aa8865409567fa3
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-07-06 16:04:28 +0100

    scoringSystem v3

--- lp1_projetoFinal/GameScoreFiles.cs
+++ lp1_projetoFinal/GameScoreFiles.cs
@@ -25,50 +25,59 @@ namespace lp1_projetoFinal
         /// <returns> return current score </returns>
         public int GetScore()
         {
-            score = 0;
-
-            if (player.health == 0)
+            score = 1;
+            /*
+            if (board.cells[board.player.position.Row, board.player.position.Col] == 
+                board.cells[board.exit.Position.Row, board.exit.Position.Col] ||
+                player.health == 0)
             {
                 //score = (1 + 0.4 * gameDifficulty) * (level + 0.1 * enemiesKilledInGame); ----> Formula do stor)
                 score++;
-            }
+            }*/
             return score;  
         }
         
 
         /// <summary>
-        /// Get results to show on score Page
+        /// Get results to show on score Page and save it to scoreList on stack
         /// </summary>
         public void ShowScoreOnPage()
         {
             Stack<int> scoreList = new Stack<int>();
+            //scoreList.Push(GetScore());
 
+            foreach (int element in scoreList)
+            {
+                scoreList.Push(score);
+            }
             for (int i = 1; i <= 8; i++)
             {
-                
-                //Console.WriteLine($"Place {i}: {GetScore()}");
-
+                Console.WriteLine($"Place {i}= {scoreList}");
             }
+
+            SaveScoreOnFile(scoreList);
         }
 
+        public void SaveScoreOnFile(Stack<int> scoreStack)
+        {
+            int.TryParse(scoreStack);
+            fileName = string.Format(@"Score_{0}{1}.txt",GameBoard.RowSize, GameBoard.ColSize);
 
+            File.WriteAllLines(fileName, scoreStack);
+        }
+
+        /*
         /// <summary>
         /// Make a new file and save score Stack into it
         /// </summary>
         /// <param name="scoreList"> get score from list</param>
         public void SaveScoreOnFile()
         {
-            string[] arrList;
-            fileName = string.Format(@"GameScore_{0}x{1}", GameBoard.RowSize, GameBoard.ColSize);
-
-
             
+            fileName = string.Format(@"Score_{0}x{1}", GameBoard.RowSize, GameBoard.ColSize);
 
-            
-
-            //File.WriteAllLines(fileName, scoreList);
-
-        }
+            File.WriteAllLines(fileName, );
+        }*/
 
 
         /// <summary>
@@ -76,6 +85,15 @@ namespace lp1_projetoFinal
         /// </summary>
         public void LoadScoreFromFile()
         {
+            List<string> scoreFiles = new List<string>();
+
+            string answer = Console.ReadLine();
+
+            foreach (int file in answer)
+            {
+                
+            }
+            
             File.ReadAllLines(fileName);
         }
     }
